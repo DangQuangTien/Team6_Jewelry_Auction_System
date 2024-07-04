fbd1e76e53d2d80a443b461120cf0e9c3c0792fb
DangQuangTien <tiendqse171443@fpt.edu.vn>
2024-07-04 22:16:53 +0700

    Revert "Update Invoice.java"
    This reverts commit aec98c1db498a5d3c502a24655cdb86db1790a2a.

--- Jewelry_Auction_Platform/src/java/entity/Invoice/Invoice.java
+++ Jewelry_Auction_Platform/src/java/entity/Invoice/Invoice.java
@@ -2,28 +2,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package entity.invoice;
+package entity.Invoice;
 
-import java.time.LocalDateTime;
+import java.math.BigDecimal;
 
 /**
  *
- * @author T14
+ * @author User
  */
 public class Invoice {
+
     private String invoiceID;
-    private LocalDateTime invoiceDate;
-    private double totalAmount;
+    private String memberID;
+    private String jewelryID;
+    private String invoiceDate;
+    private BigDecimal totalAmount;
+    private String firstName;
+    private String lastName;
+    private String jewelryName;
+    private String photos;
 
     public Invoice() {
     }
 
-    public Invoice(String invoiceID, LocalDateTime invoiceDate, double totalAmount) {
+    public Invoice(String invoiceID, String memberID, String jewelryID, String invoiceDate, BigDecimal totalAmount, String firstName, String lastName, String jewelryName, String photos) {
         this.invoiceID = invoiceID;
+        this.memberID = memberID;
+        this.jewelryID = jewelryID;
         this.invoiceDate = invoiceDate;
         this.totalAmount = totalAmount;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jewelryName = jewelryName;
+        this.photos = photos;
+    }
+    
+    
+    // Constructor
+    public Invoice(String invoiceID, String memberID, String jewelryID, String invoiceDate, BigDecimal totalAmount) {
+        this.invoiceID = invoiceID;
+        this.memberID = memberID;
+        this.jewelryID = jewelryID;
+        this.invoiceDate = invoiceDate;
+        this.totalAmount = totalAmount;
+        
     }
 
+    // Getters and Setters
     public String getInvoiceID() {
         return invoiceID;
     }
@@ -32,19 +57,69 @@ public class Invoice {
         this.invoiceID = invoiceID;
     }
 
-    public LocalDateTime getInvoiceDate() {
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
+
+    public String getJewelryID() {
+        return jewelryID;
+    }
+
+    public void setJewelryID(String jewelryID) {
+        this.jewelryID = jewelryID;
+    }
+
+    public String getInvoiceDate() {
         return invoiceDate;
     }
 
-    public void setInvoiceDate(LocalDateTime invoiceDate) {
+    public void setInvoiceDate(String invoiceDate) {
         this.invoiceDate = invoiceDate;
     }
 
-    public double getTotalAmount() {
+    public BigDecimal getTotalAmount() {
         return totalAmount;
     }
 
-    public void setTotalAmount(double totalAmount) {
+    public void setTotalAmount(BigDecimal totalAmount) {
         this.totalAmount = totalAmount;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJewelryName() {
+        return jewelryName;
+    }
+
+    public void setJewelryName(String jewelryName) {
+        this.jewelryName = jewelryName;
+    }
+
+    public String getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+    
+    
 }
