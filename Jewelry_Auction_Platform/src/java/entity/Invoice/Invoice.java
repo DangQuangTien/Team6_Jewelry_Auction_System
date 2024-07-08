5b436cb286747f28c9cdddc405aeabb2f9d5541b
DangQuangTien <tiendqse171443@fpt.edu.vn>
2024-07-08 09:35:57 +0700

    d

--- Jewelry_Auction_Platform/src/java/entity/Invoice/Invoice.java
+++ Jewelry_Auction_Platform/src/java/entity/Invoice/Invoice.java
@@ -2,53 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package entity.Invoice;
+package entity.invoice;
 
-import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  *
- * @author User
+ * @author T14
  */
 public class Invoice {
-
     private String invoiceID;
-    private String memberID;
-    private String jewelryID;
-    private String invoiceDate;
-    private BigDecimal totalAmount;
-    private String firstName;
-    private String lastName;
-    private String jewelryName;
-    private String photos;
+    private LocalDateTime invoiceDate;
+    private double totalAmount;
 
     public Invoice() {
     }
 
-    public Invoice(String invoiceID, String memberID, String jewelryID, String invoiceDate, BigDecimal totalAmount, String firstName, String lastName, String jewelryName, String photos) {
+    public Invoice(String invoiceID, LocalDateTime invoiceDate, double totalAmount) {
         this.invoiceID = invoiceID;
-        this.memberID = memberID;
-        this.jewelryID = jewelryID;
         this.invoiceDate = invoiceDate;
         this.totalAmount = totalAmount;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.jewelryName = jewelryName;
-        this.photos = photos;
-    }
-    
-    
-    // Constructor
-    public Invoice(String invoiceID, String memberID, String jewelryID, String invoiceDate, BigDecimal totalAmount) {
-        this.invoiceID = invoiceID;
-        this.memberID = memberID;
-        this.jewelryID = jewelryID;
-        this.invoiceDate = invoiceDate;
-        this.totalAmount = totalAmount;
-        
     }
 
-    // Getters and Setters
     public String getInvoiceID() {
         return invoiceID;
     }
@@ -57,69 +32,19 @@ public class Invoice {
         this.invoiceID = invoiceID;
     }
 
-    public String getMemberID() {
-        return memberID;
-    }
-
-    public void setMemberID(String memberID) {
-        this.memberID = memberID;
-    }
-
-    public String getJewelryID() {
-        return jewelryID;
-    }
-
-    public void setJewelryID(String jewelryID) {
-        this.jewelryID = jewelryID;
-    }
-
-    public String getInvoiceDate() {
+    public LocalDateTime getInvoiceDate() {
         return invoiceDate;
     }
 
-    public void setInvoiceDate(String invoiceDate) {
+    public void setInvoiceDate(LocalDateTime invoiceDate) {
         this.invoiceDate = invoiceDate;
     }
 
-    public BigDecimal getTotalAmount() {
+    public double getTotalAmount() {
         return totalAmount;
     }
 
-    public void setTotalAmount(BigDecimal totalAmount) {
+    public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
     }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getJewelryName() {
-        return jewelryName;
-    }
-
-    public void setJewelryName(String jewelryName) {
-        this.jewelryName = jewelryName;
-    }
-
-    public String getPhotos() {
-        return photos;
-    }
-
-    public void setPhotos(String photos) {
-        this.photos = photos;
-    }
-    
-    
 }
